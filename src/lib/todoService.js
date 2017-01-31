5da1ecbeb748f4d611c3894646427fe6da14e395
Siddalingesha D S <siddhudev813@gmail.com>
2017-01-31 13:09:35 -0800

    delete a todo on server

--- src/lib/todoService.js
+++ src/lib/todoService.js
@@ -27,3 +27,13 @@ export const saveTodo = (todo) => {
 	}).then(res => res.json())
 }
 
+export const destroyTodo = (id) => {
+	return fetch(`${baseUrl}/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+	})
+}
+
